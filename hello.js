a2ee9c4fa53d247c0de0eb3bb970e6e2c179f1ec
kevinng88 <gstcollection@hotmail.com>
2017-08-28 03:41:48 +0800

    surface 2

--- hello.js
+++ hello.js
@@ -1,10 +1,4 @@
 //I am changing
-<<<<<<< HEAD
-console.log("hello me!");
-console.log("hello again!");
-||||||| merged common ancestors
-console.log("hello me!");
-=======
 console.log("hello me!");
 console.log("this is ending!");
->>>>>>> 04c533be3eec20f6c778a3d87608dbe2a113fdaf
+
